77c1b6a61ad750d619e57e0087e8ec6bf95194e9
parvin <parvinnoori0@gmail.com>
2024-07-11 23:03:21 +0330

    update

--- src/router.jsx
+++ src/router.jsx
@@ -14,54 +14,51 @@ import { CategoryProvider } from "./features/categories/category-context";
 import UnhandledException from "./pages/unhandled-exception";
 import NotFound from "./pages/not-found";
 
-const router = createBrowserRouter({
-  basename: "/admin-dashboard", // Set your repository name as the basename
-  routes: [
-    {
-      path: "/",
-      element: <MainLayout />,
-      errorElement: <UnhandledException />,
-      children: [
-        {
-          element: <Courses />,
-          index: true,
-          loader: coursesList,
-        },
-        {
-          path: "/course-categories",
-          element: (
-            <CategoryProvider>
-              <CourseCategories />
-            </CategoryProvider>
-          ),
-          loader: categoriesLoader,
-        },
-        {
-          path: "courses/:id",
-          element: <CourseDetails />,
-          loader: courseDetailsLoader,
-        },
-      ],
-    },
-    {
-      element: <IdentityLayout />,
-      children: [
-        {
-          element: <Login />,
-          path: "/login",
-          action: loginAction,
-          errorElement: <Login />,
-        },
-        {
-          element: <Register />,
-          path: "/register",
-          action: registerAction,
-          errorElement: <Register />,
-        },
-      ],
-    },
-    { path: "*", element: <NotFound /> },
-  ],
-});
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <UnhandledException />,
+    children: [
+      {
+        element: <Courses />,
+        index: true,
+        loader: coursesList,
+      },
+      {
+        path: "/course-categories",
+        element: (
+          <CategoryProvider>
+            <CourseCategories />
+          </CategoryProvider>
+        ),
+        loader: categoriesLoader,
+      },
+      {
+        path: "courses/:id",
+        element: <CourseDetails />,
+        loader: courseDetailsLoader,
+      },
+    ],
+  },
+  {
+    element: <IdentityLayout />,
+    children: [
+      {
+        element: <Login />,
+        path: "/login",
+        action: loginAction,
+        errorElement: <Login />,
+      },
+      {
+        element: <Register />,
+        path: "/register",
+        action: registerAction,
+        errorElement: <Register />,
+      },
+    ],
+  },
+  { path: "*", element: <NotFound /> },
+]);
 
 export default router;
