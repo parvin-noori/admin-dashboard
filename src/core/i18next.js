44c2b2cf080fafa8abe4547061b02f97a54e0e01
parvin <parvinnoori0@gmail.com>
2024-07-13 10:09:42 +0330

    update i18next.js

--- src/core/i18next.js
+++ src/core/i18next.js
@@ -2,6 +2,21 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(Backend).use(initReactI18next).init({ lng: "fa" });
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    lng: "fa", // Default language
+    fallbackLng: "en", // Fallback language
+    debug: true,
+    ns: ["translation"], // Namespace used in your files
+    defaultNS: "translation",
+    backend: {
+      loadPath: "/admin-dashboard/locales/{{lng}}/{{ns}}.json", // Path to translation files
+    },
+    interpolation: {
+      escapeValue: false, // React already does escaping
+    },
+  });
 
-export default i18n 
+export default i18n;
